931b9497455a59b02488c6e5dd0539ed705f0183
Felipe Mendes <93352420+FelipeMendes-FM@users.noreply.github.com>
2024-02-23 13:30:44 +0000

    Update main.cpp

--- ES-synth-starter-master/src/main.cpp
+++ ES-synth-starter-master/src/main.cpp
@@ -1,47 +1,10 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <bitset>
-#include <cmath>
+#include <STM32FreeRTOS.h>
 
 //Constants
-const uint32_t interval = 100; //Display update interval
-const uint32_t sampleRate = 22000; // Sample rate in Hz
-const double baseFrequency = 440.0; // Frequency of A4 in Hz
-const double twelfthRootOfTwo = pow(2.0, 1.0 / 12.0);
-
-const double calculateFrequency(int n) {
-    return baseFrequency * pow(twelfthRootOfTwo, n - 9);
-}
-
-// Calculate step size for each note
-constexpr uint32_t calculateStepSize(double frequency) {
-    return static_cast<uint32_t>((static_cast<uint64_t>(1) << 32) * frequency / sampleRate);
-}
-
-const uint32_t stepSizes[] = {
-    calculateStepSize(calculateFrequency(0)),
-    calculateStepSize(calculateFrequency(1)),
-    calculateStepSize(calculateFrequency(2)),
-    calculateStepSize(calculateFrequency(3)),
-    calculateStepSize(calculateFrequency(4)),
-    calculateStepSize(calculateFrequency(5)),
-    calculateStepSize(calculateFrequency(6)),
-    calculateStepSize(calculateFrequency(7)),
-    calculateStepSize(calculateFrequency(8)),
-    calculateStepSize(calculateFrequency(9)), // This is A at 440Hz
-    calculateStepSize(calculateFrequency(10)),
-    calculateStepSize(calculateFrequency(11)),
-};
-
-int convertToIndex(int row, int col) {
-  return row * 4 + col;
-}
-
-const char* noteNames[] = {
-  "C", "C#", "D", "D#",
-  "E", "F", "F#", "G",
-  "G#", "A", "A#", "B"
-};
+  const uint32_t interval = 100; //Display update interval
 
 //Pin definitions
   //Row select and enable
@@ -71,8 +34,44 @@ const char* noteNames[] = {
   const int HKOW_BIT = 5;
   const int HKOE_BIT = 6;
 
+const uint32_t stepSizes[] = {
+  51076057, 
+  54113197, 
+  57330935, 
+  60740010, 
+  64351799, 
+  68178356, 
+  72232452, 
+  76527617, 
+  81078186, 
+  85899346, 
+  91007187, 
+  96418756
+  };
+
+volatile uint32_t currentStepSize;
+
+const char noteNames[12][3] = {
+  "C", "C#", "D", "D#", 
+  "E", "F", "F#", "G", 
+  "G#", "A", "A#", "B"
+  };
+
+struct {
+  std::bitset<32> inputs;
+  bool pressed = false;
+  int lastPressed;
+} sysState;
+
 //Display driver object
 U8G2_SSD1305_128X32_NONAME_F_HW_I2C u8g2(U8G2_R0);
+
+void sampleISR() {
+  static uint32_t phaseAcc = 0;
+  phaseAcc += currentStepSize;
+  int32_t Vout = (phaseAcc >> 24) - 128;
+  analogWrite(OUTR_PIN, Vout + 128);
+}
 
 //Function to set outputs using key matrix
 void setOutMuxBit(const uint8_t bitIdx, const bool value) {
@@ -87,15 +86,14 @@ void setOutMuxBit(const uint8_t bitIdx, const bool value) {
 }
 
 std::bitset<4> readCols(){
+  std::bitset<4> result;
 
-      std::bitset<4> result;
+  result[0] = digitalRead(C0_PIN);
+  result[1] = digitalRead(C1_PIN);
+  result[2] = digitalRead(C2_PIN);
+  result[3] = digitalRead(C3_PIN);
 
-      result[0] = digitalRead(C0_PIN);
-      result[1] = digitalRead(C1_PIN);
-      result[2] = digitalRead(C2_PIN);
-      result[3] = digitalRead(C3_PIN);
-
-      return result;    
+  return result;
 }
 
 void setRow(uint8_t rowIdx){
@@ -109,6 +107,84 @@ void setRow(uint8_t rowIdx){
 }
 
 
+void scanKeysTask(void * pvParameters) {
+  const TickType_t xFrequency = 50/portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  while (1) {
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+    std::bitset<32> inputs;
+    static std::bitset<32> prevInputs;
+    static int firstKeyPressed, lastKeyPress;
+
+    setRow(0);
+    delayMicroseconds(3);
+    std::bitset<4> col1 = readCols();
+    setRow(1);
+    delayMicroseconds(3);
+    std::bitset<4> col2 = readCols();
+    setRow(2);
+    delayMicroseconds(3);
+    std::bitset<4> col3 = readCols();
+
+    for (int i = 0; i < 4; i++) {
+        inputs[i] = col1[i];
+        inputs[i + 4] = col2[i];
+        inputs[i + 8] = col3[i];
+    }
+
+    if (prevInputs != inputs) {
+      std::bitset<32> change = inputs ^ prevInputs;
+      
+      for (firstKeyPressed = 0; firstKeyPressed < 12; firstKeyPressed++) {
+        if (change[0] == 1) {
+          break;
+        } else {
+          change = change >> 1;
+        }
+      }
+      lastKeyPress = firstKeyPressed;
+      uint32_t newStepSize = stepSizes[lastKeyPress];
+      __atomic_store_n(&currentStepSize, newStepSize, __ATOMIC_RELAXED);
+    }
+
+    std::bitset<32> mask = 0b00000000000000000000111111111111;
+    std::bitset<12> last12 = std::bitset<12>((inputs & mask).to_ulong());
+    if (!last12.all()) { 
+      sysState.lastPressed = lastKeyPress;
+      sysState.pressed = true;
+    } else {
+      currentStepSize = 0;
+      sysState.pressed = false;
+    }
+
+    sysState.inputs = inputs;
+    prevInputs = inputs;
+  }
+}
+
+void displayUpdateTask(void * pvParameters) {
+  const TickType_t xFrequency = 100/portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+  
+  while (1) {
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+
+    //Update display
+    u8g2.clearBuffer();         
+    u8g2.setFont(u8g2_font_ncenB08_tr); 
+    u8g2.drawStr(2,10,"Hello World!"); 
+    u8g2.setCursor(2,20);
+    u8g2.print(sysState.inputs.to_ulong(),HEX);
+    
+    if (sysState.pressed) {
+      u8g2.setCursor(2,30);
+      u8g2.print(noteNames[sysState.lastPressed]);
+    }
+    u8g2.sendBuffer();       
+    digitalToggle(LED_BUILTIN);
+  }
+}
 
 
 void setup() {
@@ -138,6 +214,33 @@ void setup() {
   u8g2.begin();
   setOutMuxBit(DEN_BIT, HIGH);  //Enable display power supply
 
+  TIM_TypeDef *Instance = TIM1;
+  HardwareTimer *sampleTimer = new HardwareTimer(Instance);
+
+  sampleTimer->setOverflow(22000, HERTZ_FORMAT);
+  sampleTimer->attachInterrupt(sampleISR);
+  sampleTimer->resume();
+
+  TaskHandle_t scanKeysHandle = NULL;
+  xTaskCreate(
+  scanKeysTask,		    /* Function that implements the task */
+  "scanKeys",		      /* Text name for the task */
+  64,      		        /* Stack size in words, not bytes */
+  NULL,			          /* Parameter passed into the task */
+  2,			            /* Task priority */
+  &scanKeysHandle );	/* Pointer to store the task handle */
+  
+  TaskHandle_t displayUpdateHandle = NULL;
+  xTaskCreate(
+  displayUpdateTask,		  /* Function that implements the task */
+  "displayUpdate",		    /* Text name for the task */
+  64,      		            /* Stack size in words, not bytes */
+  NULL,			              /* Parameter passed into the task */
+  1,			                /* Task priority */
+  &displayUpdateHandle );	/* Pointer to store the task handle */
+  
+  vTaskStartScheduler();
+
   //Initialise UART
   Serial.begin(9600);
   Serial.println("Hello World");
@@ -145,39 +248,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  static uint32_t next = millis();
-  static uint32_t count = 0;
-  std::bitset<32> inputs;
-  volatile uint32_t currentStepSize = 0;
-
-  while (millis() < next); // Wait for next interval
-
-  next += interval;
-
-  for (uint8_t row = 0; row < 3; ++row) {
-    setRow(row); // Set the current row
-    auto colBits = readCols(); // Read the columns for the current row
-
-    for (uint8_t col = 0; col < 4; ++col) {
-      // Calculate the bit index for inputs and set it
-      // Note: Adjust the formula according to your matrix wiring and logic
-      inputs[row * 4 + col] = colBits[col];
-      int index = convertToIndex(row, col);
-    }
-    
-  }
-
-
-      // Update display with the selected note
-      u8g2.clearBuffer();
-      u8g2.setFont(u8g2_font_ncenB08_tr);
-      u8g2.setCursor(2,24);
-      u8g2.print("Note: ");
-      u8g2.print(index); // Convert index to string before printing
-      u8g2.sendBuffer(); // Send buffer to display
-
-
-
-
-  // You can toggle an LED or do other tasks below...
 }
+
